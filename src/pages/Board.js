9683fadf05552fd91714c55e61b8e45b793e7779
Lucas Fonseca <lucasrfon@gmail.com>
2023-04-28 01:39:15 -0300

    refactor: changes div structure

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -2,38 +2,20 @@ import styled from "styled-components";
 import map from "../images/map.png";
 
 export default function Board() {
+  const cardSquares = Array.from({ length: 29 }, (index) => <div key={index} onClick={getEventCard}></div>);
+
+  function getEventCard () {
+    alert("Pegou carta")
+  }
+
   return (
     <Container>
-      <Capture></Capture>
-      <Card></Card>
-      <Capture></Capture>
+      <Capture>
+      </Capture>
+      <Card>
+        {cardSquares}
+      </Card>
       <City></City>
-      <Card></Card>
-      <Capture></Capture>
-      <Capture></Capture>
-      <Card></Card>
-      <Capture></Capture>
-      <Capture></Capture>
-      <Capture></Capture>
-      <City></City>
-      <Card></Card>
-      <Capture></Capture>
-      <Card></Card>
-      <Capture></Capture>
-      <Capture></Capture>
-      <Capture></Capture>
-      <Card></Card>
-      <Capture></Capture>
-      <Capture></Capture>
-      <Card></Card>
-      <City></City>
-      <City></City>
-      <City></City>
-      <City></City>
-      <City></City>
-      <City></City>
-      <City></City>
-      <Capture></Capture>
       <img src={map} alt="tabuleiro" />
     </Container>
   );
@@ -132,37 +114,184 @@ const Capture = styled.div`
     top: 246px;
     left: 588px;
   }
+
+  &:nth-child(18) {
+    top: 246px;
+    left: 684px;
+  }
+
+  &:nth-child(20) {
+    top: 122px;
+    left: 738px;
+  }
+
+  &:nth-child(21) {
+    top: 128px;
+    left: 808px;
+  }
 `;
 
 const Card = styled.div`
   position: absolute;
-  height: 38px;
-  width: 38px;
-  border-radius: 50%;
-  background-color: rgba(0, 0, 0, 0.5);
+  top: 0;
+  left: 0;
+  height: 950px;
+  width: 1688.88px;
 
-  &:nth-child(2) {
+  div {
+    position: absolute;
+    height: 38px;
+    width: 38px;
+    border-radius: 50%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  div:hover {
+    cursor: pointer;
+  }
+
+  div:nth-child(1) {
     top: 534px;
     left: 456px;
   }
 
-  &:nth-child(5) {
+  div:nth-child(2) {
     top: 418px;
     left: 464px;
   }
 
-  &:nth-child(8) {
+  div:nth-child(3) {
     top: 322px;
     left: 438px;
   }
 
-  &:nth-child(13) {
+  div:nth-child(4) {
     top: 186px;
     left: 488px;
   }
 
-  &:nth-child(15) {
+  div:nth-child(5) {
     top: 170px;
     left: 546px;
+  }
+
+  div:nth-child(6) {
+    top: 166px;
+    left: 698px;
+  }
+
+  div:nth-child(7) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(8) {
+    top: 264px;
+    left: 916px;
+  }
+
+  div:nth-child(9) {
+    top: 188px;
+    left: 1044px;
+  }
+
+  div:nth-child(10) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(11) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(12) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(13) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(14) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(15) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(16) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(17) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(18) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(19) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(20) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(21) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(22) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(23) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(24) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(25) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(26) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(27) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(28) {
+    top: 214px;
+    left: 824px;
+  }
+
+  div:nth-child(29) {
+    top: 214px;
+    left: 824px;
   }
 `
