b6e79e2312fc2703511d07f535adf90de8068464
Lucas Fonseca <lucasrfon@gmail.com>
2023-06-05 15:47:31 -0300

    feat: sets players navigation through cities

--- src/components/squares/CitySquares.js
+++ src/components/squares/CitySquares.js
@@ -31,7 +31,26 @@ export default function CitySquares({ id, position, players }) {
          <Squares
             id={id}
             onClick={(event) => turnPoke(position)}
-         ></Squares>
+         >
+            {players[0].position === position ? (
+               <Player player={players[0].player}></Player>
+            ) : null}
+            {players[1].position === position ? (
+               <Player player={players[1].player}></Player>
+            ) : null}
+            {players[2].position === position ? (
+               <Player player={players[2].player}></Player>
+            ) : null}
+            {players[3].position === position ? (
+               <Player player={players[3].player}></Player>
+            ) : null}
+            {players[4].position === position ? (
+               <Player player={players[4].player}></Player>
+            ) : null}
+            {players[5].position === position ? (
+               <Player player={players[5].player}></Player>
+            ) : null}
+         </Squares>
       );
    }
 }
