e0a35414d05d84e12adb3ee5d2b4a2c4415dac15
Lucas Fonseca <lucasrfon@gmail.com>
2023-06-05 15:56:45 -0300

    feat: sets players navigation through all squares

--- src/components/squares/TakeSquares.js
+++ src/components/squares/TakeSquares.js
@@ -1,13 +1,39 @@
-import styled from "styled-components"
-import { turnPoke } from "../../logic/fuctions"
+import styled from "styled-components";
+import { turnPoke } from "../../logic/fuctions.js";
+import Player from "../Players.js";
 
 export default function TakeSquares({ position, players }) {
-  return (<Squares onClick={(event) => turnPoke(position)}></Squares>)
+  return (
+    <Squares onClick={(event) => turnPoke(position)}>
+      {players[0].position === position ? (
+        <Player player={players[0].player}></Player>
+      ) : null}
+      {players[1].position === position ? (
+        <Player player={players[1].player}></Player>
+      ) : null}
+      {players[2].position === position ? (
+        <Player player={players[2].player}></Player>
+      ) : null}
+      {players[3].position === position ? (
+        <Player player={players[3].player}></Player>
+      ) : null}
+      {players[4].position === position ? (
+        <Player player={players[4].player}></Player>
+      ) : null}
+      {players[5].position === position ? (
+        <Player player={players[5].player}></Player>
+      ) : null}
+    </Squares>
+  );
 }
 
 const Squares = styled.div`
-  position: absolute;
-  height: 38px;
-  width: 38px;
-  border-radius: 50%;
-`
+	position: absolute;
+	height: 38px;
+	width: 38px;
+	border-radius: 50%;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
+	align-items: center;
+`;
